fbadef549dadd23a565c478e5892aeab00be692e
Niclas Heide <niclas.heide@students.tbs1.de>
2023-06-06 16:51:55 +0200

    New - Added Daisy UI plugin

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,4 @@
-import yaml from "../../node_modules/yaml/browser/index.js";
+import yaml from "../../node_modules/yaml/browser/index.js"
 //import yaml from 'yaml'
 class sideMenu extends HTMLElement {
     constructor() {
@@ -34,9 +34,9 @@ class sideMenu extends HTMLElement {
                                         <h2 class="text-2xl font-semibold leading-6 text-slate-600 text-center " id="slide-over-title">Dev Tools</h2>
                                     </div>
                                     <div class="relative mt-6 flex-1 px-4 sm:px-6">
-                                        <input type="file" class="" />
+                                        <input type="file" class="file-input " />
                                         <div class="m-3">
-                                            <button class="showXML bg-slate-400 hover:bg-slate-500 text-white font-bold py-1.5 px-4 rounded-md" disabled>
+                                            <button class="showXML btn bg-slate-400 hover:bg-slate-500 text-white font-bold py-1.5 px-4 rounded-md disabled:opacity-25" disabled>
                                                 Editieren
                                             </button>
                                         </div>
@@ -66,8 +66,9 @@ class sideMenu extends HTMLElement {
 
                 document.body.appendChild(modal);
                 modal.toggleModal();
-                
+
                 this.closeSidebar();
+                this.createForm(modal);
 
             };
             reader.readAsText(file);
@@ -91,6 +92,28 @@ class sideMenu extends HTMLElement {
         panel.classList.add("translate-x-0");
         panel.classList.remove("translate-x-full");
     }
+    createForm(modal) {
+        let output = [];
+        Object.entries(modal.content).forEach(([key, value]) => {
+            console.log(key, value);
+            const firstLetter = key.charAt(0).toUpperCase();
+            key = firstLetter + key.slice(1);
+
+            output.push(
+                `
+                
+               <label class="card w-96 bg-base-100 my-2">
+                    <div class="card-body">
+                        <span class="card-title">${key}:</span>
+                        <textarea class="textarea textarea-bordered textarea-sm w-full max-w-xs">${value}</textarea>
+                    </div>
+                </label>
+                <div class="divider"></div>
+                `
+            )
+        });
+        document.querySelector("#configForm").innerHTML = output.join("");
+    }
 }
 customElements.define('side-menu', sideMenu);
 
@@ -100,7 +123,7 @@ class yamlModal extends HTMLElement {
     }
     connectedCallback() {
         this.innerHTML = `
-        <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center z-20">
+        <div class="yaml-modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center z-20">
             <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
             <div class="modal-container w-11/12 md:max-w-md sm:max-h-[80vh] mx-auto shadow-lg z-50 overflow-y-auto scrollbar">
             <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
@@ -110,28 +133,23 @@ class yamlModal extends HTMLElement {
                 <span class="text-sm">(Esc)</span>
             </div>
             <div class="modal-content bg-white py-4 text-left px-6 border-2 border-slate-300 shadow-sm rounded-md" >
-                <!--Title-->
                 <div class="flex justify-between items-center pb-4 border-b border-slate-300" >
-                <div class="flex justify-between items-center ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
-                    </svg>
-                    <p class="text-2xl text-center font-semibold">Editor</p>
-                </div>
+                    <div class="flex justify-between items-center ">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
+                        </svg>
+                        <p class="text-2xl text-center font-semibold">Editor</p>
+                    </div>
 
-                <div class="modal-close cursor-pointer z-50 p-1 border border-inherit border-slate-300 rounded-md hover:bg-gray-100 text-netzfactor hover:text-netzfactor-light">
-                    <svg class="fill-current text-semibold" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                    <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                    </svg>
+                    <div class="modal-close cursor-pointer z-50 p-1 border border-inherit border-slate-300 rounded-md hover:bg-gray-100 text-netzfactor hover:text-netzfactor-light">
+                        <svg class="fill-current text-semibold" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                        </svg>
+                    </div>
                 </div>
-                </div>
-
+                
                 <!--Body-->
-                <form id="configForm" class="py-3">
-                    <label class="my-3 p-2 border border-slate-300 rounded-md shadow-sm flex flex-col justify-center text-lg font-medium ">
-                        <span>"Datei Name"</span>
-                        <textarea name="comment" class="shadow-sm border border-slate-200 rounded-md p-1 h-24 resize-none scrollbar" placeholder="Kommentar zum Urlaub"></textarea>
-                    </label>
+                <form id="configForm" class="py-3 flex flex-col border-opacity-50">
                 </form>
                 <!--Footer-->
                 <div class="flex justify-end border-t border-slate-300 pt-4">
@@ -181,7 +199,7 @@ class yamlModal extends HTMLElement {
     toggleModal() {
         console.log("toggleModal");
         const body = document.querySelector('body');
-        const modal = document.querySelector('.modal');
+        const modal = document.querySelector('.yaml-modal');
         modal.classList.toggle('opacity-0');
         modal.classList.toggle('pointer-events-none');
         body.classList.toggle('modal-active');
